c9490b4fdcae7cfc26248166b28bf878d4adfa21
Ravivarma Singaravelu <raviamrav@yahoo.com>
2026-04-04 13:42:28 +0200

    Fix backend render url

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -5,7 +5,7 @@ let API_URL;
 if (hostname === 'localhost' || hostname === '127.0.0.1') {
   API_URL = 'http://localhost:8000';
 } else if (hostname.includes('vercel.app') || hostname.includes('priyam-foods')) {
-  API_URL = 'https://priyam-foods-backend.onrender.com';
+  API_URL = 'https://priyamfoods-backend.onrender.com';
 } else {
   API_URL = `${window.location.protocol}//${hostname}:8000`;
 }
